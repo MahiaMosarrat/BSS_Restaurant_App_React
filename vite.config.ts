8d255c7cd9ac8654216cf85e80f2e175a13099ed
Mahia Mosarrat <mahiarite@gmail.com>
2026-02-03 09:54:28 +0600

    fix new

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,32 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   base: process.env.VITE_BASE_PATH || "/restaurant_app_bss-v1",
-//   plugins: [
-//     react({
-//       babel: {
-//         plugins: [['babel-plugin-react-compiler']],
-       
-//       },
-//     }),
-//   ],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => {
-  return {
-
-    base: command === 'serve' ? '/' : '/restaurant_app_bss-v1/',
-    plugins: [
-      react({
-        babel: {
-          plugins: [['babel-plugin-react-compiler']],
-        },
-      }),
-    ],
-  }
+// https://vite.dev/config/
+export default defineConfig({
+  base: process.env.BASE_PATH || "/BSS_Restaurant_App_React",
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+       
+      },
+    }),
+  ],
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig(({ command }) => {
+//   return {
+
+//     base: command === 'serve' ? '/' : '/restaurant_app_bss-v1/',
+//     plugins: [
+//       react({
+//         babel: {
+//           plugins: [['babel-plugin-react-compiler']],
+//         },
+//       }),
+//     ],
+//   }
+// })
